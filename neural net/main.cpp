da90d12b580dc9c1af9011afe73b8f3a7dfe61e9
Greg <engineering.supergenius@gmail.com>
2020-07-03 14:22:45 -0400

    Initial Commit

---
+++ neural net/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  neural net
+//
+//  Created by Greg Lewin on 7/3/20.
+//  Copyright © 2020 Greg Lewin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
